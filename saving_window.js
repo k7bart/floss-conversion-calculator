c2060342c3a76b1b3c498cc84d9663a37f3995af
Kateryna Bartienieva <k7bart@gmail.com>
2023-07-18 00:09:37 +0300

    formated the saving_window

--- saving_window.js
+++ saving_window.js
@@ -2,58 +2,59 @@ import { Collection } from "./collection.js";
 import { conversionContainer } from "./conversion_container.js";
 import { savedCollections } from "./saved_collections.js";
 
-export const savingWindow = {
-    innerHTML: `
-        <button class="close_button">
-            <span class="material-symbols-outlined"> close </span>
-        </button>
-        <input
-            id="enter_name_input"
-            maxlength="100"
-            placeholder="Enter the name of the list"
-            name="ConvertFromCode"
-        />
-        <button class="button" type="submit">
-            <img class="image favorite_image" src="images/favorite.svg" alt="" />
-        </button>`,
+export function addSavingWindow(findedColors, savedLists) {
+    renderSavingWindow();
+    addEventListenerToSavingWindow(findedColors, savedLists);
+}
 
-    render() {
-        const savingWindowElement = document.createElement("div");
-        savingWindowElement.id = "saving_window";
-        savingWindowElement.innerHTML = savingWindow.innerHTML;
-        document.body.appendChild(savingWindowElement);
-    },
+function renderSavingWindow() {
+    const savingWindowElement = document.createElement("div");
+    savingWindowElement.id = "saving_window";
+    savingWindowElement.innerHTML = `
+    <button class="close_button">
+    <img class="close_image" src="images/close.svg" alt="close" />
+    </button>
+    <input
+        id="enter_name_input"
+        maxlength="100"
+        placeholder="Enter the name of the list"
+        name="ConvertFromCode"
+    />
+    <button class="button" type="submit">
+        <img class="image favorite_image" src="images/favorite.svg" alt="" />
+    </button>`;
+    document.body.appendChild(savingWindowElement);
+}
 
-    addEventListenerToSavingWindow(findedColors, savedLists) {
-        const savingWindowElement = document.getElementById("saving_window");
+function addEventListenerToSavingWindow(findedColors, savedLists) {
+    const savingWindowElement = document.getElementById("saving_window");
 
-        savingWindowElement.addEventListener("click", function (event) {
-            let listOfColorsToSave = [...findedColors];
+    savingWindowElement.addEventListener("click", function (event) {
+        let listOfColorsToSave = [...findedColors];
 
-            const enterNameInput =
-                savingWindowElement.querySelector("#enter_name_input");
+        const enterNameInput =
+            savingWindowElement.querySelector("#enter_name_input");
 
-            const closeButton = event.target.closest(".close_button");
-            const saveButton = event.target.closest(".button");
+        const closeButton = event.target.closest(".close_button");
+        const saveButton = event.target.closest(".button");
 
-            if (!closeButton && !saveButton) return;
+        if (!closeButton && !saveButton) return;
 
-            if (closeButton) return savingWindowElement.remove();
+        if (closeButton) return savingWindowElement.remove();
 
-            if (saveButton) {
-                const collection = new Collection(
-                    enterNameInput.value,
-                    listOfColorsToSave,
-                    conversionContainer.convertFrom,
-                    conversionContainer.convertTo
-                );
-                savedLists.push(collection);
+        if (saveButton) {
+            const collection = new Collection(
+                enterNameInput.value,
+                listOfColorsToSave,
+                conversionContainer.convertFrom,
+                conversionContainer.convertTo
+            );
+            savedLists.push(collection);
 
-                savedCollections.render(collection);
+            savedCollections.render(collection);
 
-                savingWindowElement.remove();
-                return;
-            }
-        });
-    },
-};
+            savingWindowElement.remove();
+            return;
+        }
+    });
+}
